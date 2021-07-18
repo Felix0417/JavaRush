99e8eb97a678949e71cfcbec855102e9c87b7336
Felix <felix.the.cat.msk@gmail.com>
2021-07-18 03:41:38 +0300

    .

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) throw new TooShortStringException();
+        String str = "";
+        String[] strings = string.split("\t");
+        if (strings.length < 3) throw new TooShortStringException();
+        str = strings[1];
+        return str;
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}

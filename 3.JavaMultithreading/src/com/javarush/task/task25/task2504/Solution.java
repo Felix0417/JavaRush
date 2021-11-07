1e3bde85d83eec2180657c062a27ae8eb4ad3a64
Felix <felix.the.cat.msk@gmail.com>
2021-11-07 21:08:15 +0300

    В методе processThreads() обратил множество нитей в цикл и каждую обработал switch...case, с указанием обработки приходящего состояния нити.

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -4,11 +4,34 @@ package com.javarush.task.task25.task2504;
 Switch для нитей
 */
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution extends Thread {
     public static void processThreads(Thread... threads) {
         //implement this method - реализуйте этот метод
+        for (Thread thread : threads) {
+            switch (thread.getState()){
+                case NEW:
+                    thread.start();
+                    break;
+                case WAITING:
+                case TIMED_WAITING:
+                case BLOCKED:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                    thread.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
+            }
+        }
     }
 
     public static void main(String[] args) {
+
     }
 }

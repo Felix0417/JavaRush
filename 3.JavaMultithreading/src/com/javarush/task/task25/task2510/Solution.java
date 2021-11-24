08e4a11b445b011bec98e348448c8092b8e9a145
Мишаня <felix.the.cat.msk@gmail.com>
2021-11-24 22:20:00 +0300

    1. В конструкторе определил вызов метода setUncaughtExceptionHandler() с анонимным классом Thread.UncaughtExceptionHandler(), в котором определил вывод в консоль того варианта, который содержит в себе ошибку типа Error, исключение Exception или исключение Throwable. 2. Для проверки в методе main создал нить и запустил ее. При входе нити в метод run(), помимо вызова метода суперкласса, добавил выброс исключения.

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -7,8 +7,35 @@ package com.javarush.task.task25.task2510;
 public class Solution extends Thread {
 
     public Solution() {
+        this.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                if (e instanceof Error){
+                    System.out.println("Нельзя дальше работать");
+                }else if (e instanceof Exception){
+                    System.out.println("Надо обработать");
+                }else if (e instanceof Throwable){
+                    System.out.println("Поживем - увидим");
+                }
+            }
+        });
+
+        }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            getUncaughtExceptionHandler().uncaughtException(this, e);
+        }
     }
 
+
     public static void main(String[] args) {
+        Thread thread = new Solution();
+
+        thread.start();
     }
 }

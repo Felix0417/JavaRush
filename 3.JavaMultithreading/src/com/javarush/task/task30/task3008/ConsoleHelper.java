4f368af5ff1386a876a7f53ae9600e99fc9e972f
Felix <felix.the.cat.msk@gmail.com>
2022-02-14 23:40:54 +0300

    1. В классе ConsoleHelper создал статический объект типа BufferedReader с вводом данных с консоли. 2. Создал статический метод writeMessage, который принимает на вход строку и выводит ее на консоль. 3. Создал метод readString(), который читает данные с консоли в цикле: 3.1. При удачной попытке чтения производится прерывание цикла, и метод возвращает строку. 3.2. При возникновении исключения на консоль выводится сообщение и цикл начинается снова, до получения корректного значения(п.3.1.). 4. Создал метод readInt(), который читает данные из метода readString() и преобразует их в значение int: 4.1. При удачной попытке чтения производится прерывание цикла, и метод возвращает число (int). 4.2. При возникновении исключения на консоль выводится сообщение и цикл начинается снова, до получения корректного значения(п.4.1.).

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,42 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String readFromConsole;
+        while (true){
+            try{
+                readFromConsole = reader.readLine();
+                break;
+            }catch (IOException e){
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return readFromConsole;
+
+    }
+
+    public static int readInt(){
+        int readIntFromReadString;
+        while (true){
+            try {
+                readIntFromReadString = Integer.parseInt(readString());
+                break;
+            }catch (NumberFormatException e){
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return readIntFromReadString;
+    }
+
 }

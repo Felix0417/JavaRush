79d8616a1f4de57643fe7314e7f4e89c9fc495f5
Felix <felix.the.cat.msk@gmail.com>
2022-02-15 22:44:10 +0300

    1. В классе Server создал вложенный класс Handler, унаследованный от Thread. 2. В классе Handler объявил переменную типа Socket, объявил для него конструктор. 3. В методе main класса Server выполнил следующие действия: 3.1. Запросил данные порта у класса ConsoleHelper и на их основе создал объект типа serverSocket; 3.2. В бесконечном цикле в обрабатываемом блоке выводим ожидание клиента в виде сокета и создаем на его основе новый экземпляр класса Handler, который сразу запускаем; 3.3. При возникновении исключения закрываем Серверный сокет и прерываем цикл. Если корректное закрытие сокета не получилось - выводим сообщение в консоль

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,8 +1,38 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args) {
+    private static class Handler extends Thread {
+        Socket socket;
 
+        Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int serverSock = ConsoleHelper.readInt();
+        ServerSocket serverSocket = new ServerSocket(serverSock);
+        System.out.println("Сервер запущен!");
+
+        Socket socket;
+
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+                new Handler(socket).start();
+            } catch (Exception e) {
+                try {
+                    serverSocket.close();
+                    break;
+                }catch (Exception f) {
+                    f.printStackTrace();
+                }
+            }
+        }
     }
 }

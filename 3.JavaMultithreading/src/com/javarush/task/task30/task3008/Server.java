a642c3df5b043eaa1c7c3c4fe31ab8907d6d87c2
Мишаня <felix.the.cat.msk@gmail.com>
2022-02-16 11:34:38 +0300

    1. В классе Server обьявил статическое поле типа Map<String, Connection>, где ключ - имя, а значение - соединение. Инициализировал данную карту через ConcurrentHashMap(потокобезопасный) 2. Создал метод sendBroadcastMessage(), в котором входящее значение типа Message отправляется всем значениям Connection из карты ConcurrentHashMap через метод Connection.send(). При возникновении исключения на консоль выводится сообщение о неудачной отправке.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,21 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String, Connection> entry : connectionMap.entrySet()){
+            try {
+                entry.getValue().send(message);
+            }catch (IOException e){
+                System.out.println("не смогли отправить сообщение");
+            }
+        }
+    }
 
     private static class Handler extends Thread {
         Socket socket;
@@ -16,23 +29,23 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         int serverSock = ConsoleHelper.readInt();
-        ServerSocket serverSocket = new ServerSocket(serverSock);
-        System.out.println("Сервер запущен!");
+            ServerSocket serverSocket = new ServerSocket(serverSock);
+            System.out.println("Сервер запущен!");
 
-        Socket socket;
+            Socket socket;
 
-        while (true) {
-            try {
-                socket = serverSocket.accept();
-                new Handler(socket).start();
-            } catch (Exception e) {
+            while (true) {
                 try {
-                    serverSocket.close();
-                    break;
-                }catch (Exception f) {
-                    f.printStackTrace();
+                    socket = serverSocket.accept();
+                    new Handler(socket).start();
+                } catch (Exception e) {
+                    try {
+                        serverSocket.close();
+                        break;
+                    }catch (Exception f) {
+                        f.printStackTrace();
+                    }
                 }
             }
-        }
     }
 }

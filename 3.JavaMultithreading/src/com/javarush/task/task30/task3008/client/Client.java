c52f19872eb885a15038a1d73eec2f8e4d2e9c38
Мишаня <felix.the.cat.msk@gmail.com>
2022-02-18 13:00:56 +0300

    1. В классе Client пакета client создал метод getServerAddress(), который возвращает считанную с консоли строку из метода класса ConsoleHelper. 2. Создал метод getServerPort(), который возвращает считанное с консоли число из метода класса ConsoleHelper. 3. Создал метод getUserName(), который возвращает считанную с консоли строку из метода класса ConsoleHelper. 4. Создал метод shouldSendTextFromConsole(), который возвращает значение true. 5. Создал метод getSocketThread(), который возвращает новый обьект типа SocketThread(). 6. Создал метод sendTextMessage(), который принимает сообщение и отправляет его через connection.send(). Если возникает исключительная ситуация, отправляем сообщение об этом вызывающему и устанавливаем переменную clientConnected в значение false.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,12 +1,46 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     private volatile boolean clientConnected = false;
     protected Connection connection;
 
-    public static class SocketThread extends Thread{
+    protected String getServerAddress() {
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("ошибка");
+            clientConnected = false;
+        }
+    }
+
+    public static class SocketThread extends Thread {
 
 
     }

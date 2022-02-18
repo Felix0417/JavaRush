15e6de76ea27223b749e629106e00d1073b35f00
Мишаня <felix.the.cat.msk@gmail.com>
2022-02-18 12:26:07 +0300

    1. Создал пакет client, в нем создал класс Сlient. 2. В нем создал приватную волатильную булеву переменную clientConnected, которую инициализровал как false. 3. В нем указал на обьект connection типа Connection. 4. В классе Сlient создал внутренний публичный класс SocketThread, который унаследовал от SocketThread.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+public class Client {
+    private volatile boolean clientConnected = false;
+    protected Connection connection;
+
+    public static class SocketThread extends Thread{
+
+
+    }
+}

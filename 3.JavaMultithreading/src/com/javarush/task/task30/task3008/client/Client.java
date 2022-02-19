1d1a2b818cb857790768d743adf2011f7e89e616
Felix <felix.the.cat.msk@gmail.com>
2022-02-19 21:21:55 +0300

    1. В классе Client во внутреннем классе SocketThread создал метод clientMainLoop(), который в зависимости от типа данных совершает следующие операции: 1.1 Вызывает метод processIncomingMessage(), если типом входных данных является строка(TEXT). 1.2 Вызывает метод informAboutAddingNewUser(), если типом входных данных является добавление клиента(USER_ADDED). 1.3 Вызывает метод informAboutDeletingNewUser(), если типом входных данных является удаление клиента(USER_REMOVED). 1.4 В ином случае бросаем исключение IOException().
    2. В классе Client во внутреннем классе SocketThread создал метод clientHandshake(), который в зависимости от типа данных совершает следующие операции:
    2.1 Если тип данных является запрос имени(NAME_REQUEST) - посылаем сообщение с типом имени и само имя через метод connection.send()
    2.2 Вызывает метод informAboutDeletingNewUser(), если типом входных данных является NAME_ACCEPTED, пробуждаем нашу нить через notifyConnectionStatusChanged(), после чего прерываем цикл
    2.3 При иных входных данных генерируем новое исключение IOException.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,19 +1,12 @@
 package com.javarush.task.task30.task3008.client;
 
-import com.javarush.task.task30.task3008.Connection;
-import com.javarush.task.task30.task3008.ConsoleHelper;
-import com.javarush.task.task30.task3008.Message;
-import com.javarush.task.task30.task3008.MessageType;
+import com.javarush.task.task30.task3008.*;
 
 import java.io.IOException;
 
 public class Client {
     private volatile boolean clientConnected = false;
     protected Connection connection;
-
-    public void setClientConnected(boolean clientConnected) {
-        this.clientConnected = clientConnected;
-    }
 
     protected String getServerAddress() {
         return ConsoleHelper.readString();
@@ -92,6 +85,33 @@ public class Client {
             Client.this.clientConnected = clientConnected;
             synchronized (Client.this){
                 Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException{
+            Message message;
+            while (true){
+                message = connection.receive();
+                if (MessageType.NAME_REQUEST.equals(message.getType())){
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                }else if (MessageType.NAME_ACCEPTED.equals(message.getType())){
+                    notifyConnectionStatusChanged(true);
+                    break;
+                }else throw new IOException("Unexpected MessageType");
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException{
+            Message message;
+            while (true){
+                message = connection.receive();
+                if (MessageType.TEXT.equals(message.getType())){
+                    processIncomingMessage(message.getData());
+                }else if (MessageType.USER_ADDED.equals(message.getType())){
+                    informAboutAddingNewUser(message.getData());
+                }else if (MessageType.USER_REMOVED.equals(message.getType())){
+                    informAboutDeletingNewUser(message.getData());
+                }else throw new IOException("Unexpected MessageType");
             }
         }
 

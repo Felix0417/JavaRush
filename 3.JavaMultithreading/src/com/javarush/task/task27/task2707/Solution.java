e6657417e901cbcb10e79d29f455e211fbb7312e
Felix <felix.the.cat.msk@gmail.com>
2022-03-14 00:30:02 +0300

    Выполнение задачи производится в методе isLockOrderNormal() 1. Создаем нить1, в которой переопределяем метод run(), в нем синхронизируем блок сначала по о1, потом переводим нить в ожидание на 500мс и создаем второй синхронизированный блок по объекту о2. 2. Создаем нить2, в которой в переопределенном методе вызываем наш метод someMethodWithSynchronizedBlocks(), используя входные данные. 3. Запускаем последовательно обе нити, после чего ставим их на ожидание в течение 2 секунд. 4. Возвращаем статус блокировки второго потока.

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -15,7 +15,29 @@ public class Solution {
 
     public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        return false;
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (o1) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (o2) {
+
+                    }
+                }
+            }
+        };
+
+        Thread thread2 = new Thread(()-> solution.someMethodWithSynchronizedBlocks(o1, o2));
+
+        thread1.start();
+        thread2.start();
+        Thread.sleep(2000);
+
+        return thread2.getState() != Thread.State.BLOCKED;
     }
 
     public static void main(String[] args) throws Exception {
@@ -24,5 +46,6 @@ public class Solution {
         final Object o2 = new Object();
 
         System.out.println(isLockOrderNormal(solution, o1, o2));
+
     }
 }

93467eb7ecdef7b6837f0e365566457a2de3f496
Felix <felix.the.cat.msk@gmail.com>
2022-03-29 01:03:14 +0300

    1. В классе Archiver создал метод askOperation(), который сначала выводит все команды(перечисления) из класса Operation и запрашивает число с клавиатуры, которое будет равно одному из перечислений, и возвращает это перечисление. 2. В методе main() переписал весь код: обьявил переменную operation и в цикле при помощи метода askOperation() объявляем наш объект operation. После чего запускаем выбранную команду через CommandExecutor.execute(). Обрабатываем исключения. Если наша переменная operation не равна EXIT, то цикл будет бесконечным

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,23 +1,48 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
 
 public class Archiver {
 
+    public static Operation askOperation() throws Exception {
+        int numOfOperation;
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
+                Operation.ADD.ordinal() + " - добавить файл в архив\n" +
+                Operation.REMOVE.ordinal() + " - удалить файл из архива\n" +
+                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                Operation.EXIT.ordinal() + " - выход");
+        while (true){
+            numOfOperation = ConsoleHelper.readInt();
+            for (Operation o : Operation.values()){
+                if (numOfOperation == o.ordinal()){
+                    return o;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
-
-        Path targetToArchivision = Paths.get(reader.readLine());
-        zipFileManager.createZip(targetToArchivision);
-
-        new ExitCommand().execute();
+        Operation operation = null;
+        while (!Operation.EXIT.equals(operation)) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e){
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception en){
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
     }
 }

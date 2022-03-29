9b8d47af92dcf21134e507f63641d2d4d9038e9e
Мишаня <felix.the.cat.msk@gmail.com>
2022-03-29 13:58:48 +0300

    1. Создал класс FileManager. 2. Объявил приватную переменную rootPath, которая инициализируется через параметизированный конструктор. 3. Объявил список Path, инициализировал его через конструктор и сделал геттер. 4. Создал приватный метод collectFileList(), который работает следующим образом:  - при распознавании входного пути(path) как отдельного файла - добавляет относительный путь в список fileList;  - при распознавании входного пути(path) как каталога - создает список всех файлов директории (directoryStream) и через перебор рекурсивно вызывает метод collectFileList()

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -2,38 +2,36 @@ package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.IOException;
+
 public class Archiver {
+    public static void main(String[] args) throws IOException {
 
-    public static Operation askOperation() throws Exception {
-        int numOfOperation;
-        ConsoleHelper.writeMessage("Выберите операцию:\n" +
-                Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
-                Operation.ADD.ordinal() + " - добавить файл в архив\n" +
-                Operation.REMOVE.ordinal() + " - удалить файл из архива\n" +
-                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
-                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
-                Operation.EXIT.ordinal() + " - выход");
-        while (true){
-            numOfOperation = ConsoleHelper.readInt();
-            for (Operation o : Operation.values()){
-                if (numOfOperation == o.ordinal()){
-                    return o;
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
         Operation operation = null;
-        while (!Operation.EXIT.equals(operation)) {
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
-            } catch (WrongZipFileException e){
+            } catch (WrongZipFileException e) {
                 ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
-            } catch (Exception en){
+            } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-        }
+
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }

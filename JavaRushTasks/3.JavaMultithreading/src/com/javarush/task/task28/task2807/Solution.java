678f6931c31aea78685fa8ccd65c7270cd74a41e
felix <Felix0417>
2022-08-16 22:38:13 +0300

    1. Создал связанный список LinkedBlockingQueue. 2. Создал объект ThreadPoolExecutor, указал параметры по условию 3. В цикле из 10 итераций в список добавил нити, которые вызывают метод doExpensiveOperation() 4. Запустил методы .prestartAllCoreThreads(), .shutdown(), .awaitTermination().

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task28.task2807;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /* 
 Знакомство с ThreadPoolExecutor
@@ -11,6 +12,18 @@ import java.util.concurrent.TimeUnit;
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
         // Add your code here
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+
+        LinkedBlockingQueue<Runnable> blockingQueue= new LinkedBlockingQueue<Runnable>();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3,
+                5, 1000, TimeUnit.MILLISECONDS, blockingQueue);
+
+        for (int i = 0; i < 10; i++) {
+            blockingQueue.add(() -> doExpensiveOperation(atomicInteger.getAndIncrement()));
+        }
+        executor.prestartAllCoreThreads();
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
 
         /* Example output
 pool-1-thread-2, localId=2

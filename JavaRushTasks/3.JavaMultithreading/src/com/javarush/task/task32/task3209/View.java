5f0187749835d00b0d16dff4b8185b46aba4d2b2
felix <Felix0417>
2022-08-25 00:37:39 +0300

    1. Описал метод initEditor(). 2. Добавил новые вкладки через tabbedPane.addTab() 3. Установил размер окна, его расположение и установил нового слушателя

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,7 +36,12 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor(){
-
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(500, 500));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui(){

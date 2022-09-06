6c9294ab29fb3bf15f7316d05ddbffc9c4802a63
felix <Felix0417>
2022-09-06 23:42:54 +0300

    1. Переименовал метод recursion() на getAllNumbersBetween(). 2. В методе getAllNumbersBetween() поменял рекурсию на циклы.

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -10,15 +10,20 @@ public class Solution {
     private static int numberA;
     private static int numberB;
 
-    public static String recursion(int a, int b) {
+    public static String getAllNumbersBetween(int a, int b) {
         if (a > b) {
-            return a + " " + recursion(a - 1, b);
+            for (int i = a; i >= b; i--){
+                System.out.print(i + (i > b? " " : ""));
+            }
         } else {
             if (a == b) {
                 return Integer.toString(a);
             }
-            return a + " " + recursion(a + 1, b);
+            for (int i = a; i <= b; i++){
+                System.out.print(i + (i < b? " " : ""));
+            }
         }
+        return "";
     }
 
     public static void main(String[] args) {

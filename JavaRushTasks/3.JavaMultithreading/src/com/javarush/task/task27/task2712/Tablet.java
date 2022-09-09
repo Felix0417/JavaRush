7d092aef94225bc3eab834e3d0b0eabba28e75d4
felix <Felix0417>
2022-09-10 00:38:48 +0300

    1. Создал класс Cook, реализовал методы. 2. Дописал в класс Restaurant создание нового повара. 3. Дополнил метод createOrder() класса Tablet и изменил тип метода

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,10 +3,11 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable {
     private final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -14,12 +15,17 @@ public class Tablet {
         this.number = number;
     }
 
-    public void createOrder() throws IOException {
+    public Order createOrder() throws IOException {
         try {
-            ConsoleHelper.writeMessage(new Order(this).toString());
+            Order newOrder = new Order(this);
+            ConsoleHelper.writeMessage(newOrder.toString());
+            setChanged();
+            notifyObservers(newOrder);
+            return newOrder;
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
+        return null;
     }
 
     @Override

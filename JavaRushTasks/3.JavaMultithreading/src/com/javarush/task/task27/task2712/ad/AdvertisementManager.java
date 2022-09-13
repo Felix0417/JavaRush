722d8d9aeae6ff948dabea05ef506f6a7a7f1a9c
felix <Felix0417>
2022-09-14 00:37:22 +0300

    1. Исправил AdvertisementStorage в соответствии с заданием

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,4 +1,7 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.List;
+
 public class AdvertisementManager {
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }

f4ed61bfbfad44f3df2a178e2d0cc0ae5ebf65c1
felix <Felix0417>
2022-09-18 16:08:40 +0300

    1. В классе StatisticManager создал внутренний класс StatisticStorage и заполнил его

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.*;
 
 public class StatisticManager {
     private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {
     }
@@ -17,5 +21,15 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
 
+    }
+
+    private static class StatisticStorage {
+        Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<>());
+            }
+        }
     }
 }

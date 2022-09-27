b61281b84b2a09d24e22dc4940b1cbab95ae20bc
felix <Felix0417>
2022-09-27 12:24:10 +0300

    Написла метод getRadix(), в которо проверяется на палиндром наше переданное число. Соответствие палиндрому делегируется методу isPalindrome()

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task30.task3009;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -13,5 +14,30 @@ public class Solution {
         System.out.println(getRadix("123"));        //expected output: [6]
         System.out.println(getRadix("5321"));       //expected output: []
         System.out.println(getRadix("1A"));         //expected output: []
+    }
+
+    private static Set<Integer> getRadix(String number) {
+        Set<Integer> result = new HashSet<>();
+        try {
+            String[] nums;
+            for (int i = 2; i < 37; i++) {
+                nums = Integer.toString(Integer.parseInt(number), i).split("");
+                if (isPalindrome(nums)) {
+                    result.add(i);
+                }
+            }
+        } catch (NumberFormatException e) {
+            return new HashSet<>();
+        }
+        return result;
+    }
+
+    public static boolean isPalindrome(String[] numbers) {
+        for (int i = 0; i < numbers.length / 2; i++) {
+            if (!(Objects.equals(numbers[i], numbers[numbers.length - 1 - i]))) {
+                return false;
+            }
+        }
+        return true;
     }
 }

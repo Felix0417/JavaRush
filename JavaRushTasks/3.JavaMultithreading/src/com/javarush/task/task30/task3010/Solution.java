4349638a5e4ce61653eb5d5c534d1b14318f8ce5
felix <Felix0417>
2022-09-27 20:40:46 +0300

    Реализовал метод для нахождения минимальной системы счисления согласно условиям задачи. Для соответствия 1 пункта обернул вель код метода main в блок try...catch

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -1,13 +1,32 @@
 package com.javarush.task.task30.task3010;
 
-import java.util.regex.Pattern;
+import java.math.BigInteger;
 
 /* 
 Минимальное допустимое основание системы счисления
 */
 
 public class Solution {
+
     public static void main(String[] args) {
         //напишите тут ваш код
+        try {
+            String s = args[0];
+            int minRadix = 0;
+            for (int i = 2; i < 37; i++) {
+                try {
+                    new BigInteger(s, i);
+                    minRadix = i;
+                    break;
+                } catch (Exception ignored) {
+                }
+            }
+            if (minRadix != 0) {
+                System.out.println(minRadix);
+            } else {
+                System.out.println("incorrect");
+            }
+        } catch (Exception exception) {
+        }
     }
 }

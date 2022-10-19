cac6d62b70cbbbe4f2b5eb43f9ecbfb56405c4ea
felix <Felix0417>
2022-10-19 22:44:00 +0300

    1. Создал приватный метод getEmptyTiles(), который добавляет все пустые ячейки в отдельный список и возвращает его. 2. В классе Model создал приватный метод addTile(), который устанавливает значение Tiles.value из нашего игрового поля значением согласно алгоритму. Добавил проверку на передачу из метода пустого списка. 3. Создал метод resetGameTiles(), в который положил данные из конструктора. Дополнил вызовом метода addTile() 2 раза

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,16 +1,43 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
 
     public Model() {
+        resetGameTiles();
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (Tile[] gameTile : gameTiles) {
+            for (int j = 0; j < gameTiles.length; j++) {
+                if (gameTile[j].isEmpty()) {
+                    emptyTiles.add(gameTile[j]);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+
+    private void addTile() {
+        List<Tile> tiles = getEmptyTiles();
+        if (!tiles.isEmpty()) {
+            tiles.get((int) (tiles.size() * Math.random())).value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
+
+    protected void resetGameTiles() {
         this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles.length; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 }
